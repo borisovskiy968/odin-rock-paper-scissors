2a36e9cd57ea23719b13933f1d8d589fabc12cc0
Igor Borisovskiy <58004557+borisovskiy968@users.noreply.github.com>
2022-12-12 14:26:41 +0100

    Create a function getComputerChoice

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
+//Create a function that generates random integer from 0 to a given max number
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
+//Create a function that gets computer's choice which is random
+function getComputerChoice() {
+  //Generate random number from 0 to 2 and return either 'Rock', 'Paper' or
+  //'Scissors' correspondingly
+  switch (getRandomInt(3)) {
+    case 0:
+      return 'Rock';
+    case 1:
+      return 'Paper';
+    case 2:
+      return 'Scissors';
+  };
+}
